04dd2bf9544faa856d792f00918df4df848f4c8f
Bill McKessy <bill@pixieinteractive.io>
2022-03-21 14:54:50 +0100

    good pixi > three > pixi workflow

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { useEffect } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
-import go from './tree.js'
+import step1 from './lib/pixi-heightmap-steepmap-to-texture-id';
+import step2 from './lib/heightmap-displace-threejs'
+
+import allSteps from './lib/allSteps'
 
 function App() {
 
     useEffect(() => {
-        go()
+        allSteps()
+        // step1
+        // const app1 = step1(step2)
+        // go(app1)
     }, [])
 
     return (
         <div className="App">
+            <div className='pixi-step-1'/>
+            <div className='three-displace-step'/>
+            <div className='pixi-step-2'/>
         </div>
     );
 }
